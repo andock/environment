163f503705ca022ef5e7dcf1ffe1861bdaf105dd
Christian Wiedemann <christian.wiedemann@key-tec.de>
2017-06-17 21:44:20 +0200

    Add ssh config

--- before_install.sh
+++ before_install.sh
@@ -8,6 +8,6 @@ openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in id_rsa.enc -out ~/.s
 chmod 600 ~/.ssh/id_rsa
 eval `ssh-agent -s`
 ssh-add ~/.ssh/id_rsa
-mv -fv ssh-config ~/.ssh/config
+cp ssh-config ~/.ssh/config
 git config --global user.email "christian.wiedemann@key-tec.de"
 git config --global user.name "KEY-TEC (via TravisCI)"
