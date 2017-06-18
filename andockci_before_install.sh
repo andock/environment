92bc39bf13c300bc6ae3cd85de83b3708d12c955
Christian Wiedemann <christian.wiedemann@key-tec.de>
2017-06-18 22:51:12 +0200

    Add ssh

--- andockci_before_install.sh
+++ andockci_before_install.sh
@@ -3,9 +3,10 @@ set -e
 
 sudo cp /home/travis/.ssh/id_rsa /home/andock-ci/id_rsa
 sudo chown andock-ci:docker /home/andock-ci/.ssh/id_rsa
+sudo chmod 600 /home/andock-ci/.ssh/id_rsa
+sudo cp ssh-config /home/andock-ci/.ssh/config
+sudo chown andock-ci:docker /home/andock-ci/.ssh/config
+ssh -o StrictHostKeyChecking=no andock-ci@localhost "eval `ssh-agent -s`"
+ssh -o StrictHostKeyChecking=no andock-ci@localhost "ssh-add /home/andock-ci/.ssh/id_rsa"
 
-ssh -o StrictHostKeyChecking=no andock-ci@localhost
-chmod 600 /home/andock-ci/.ssh/id_rsa
-eval `ssh-agent -s`
-ssh-add /home/andock-ci/.ssh/id_rsa
-cp ssh-config /home/andock-ci/.ssh/config
+
