979df44653b7850c6c5c813cc9ad8b2a4dc9eeb9
Christian Wiedemann <christian.wiedemann@key-tec.de>
2017-06-18 15:53:25 +0200

    Fix tests

--- andockci_before_install.sh
+++ andockci_before_install.sh
@@ -4,7 +4,7 @@ set -e
 sudo cp /home/travis/.ssh/id_rsa /home/andock-ci/id_rsa
 sudo chown andock-ci:docker /home/andock-ci/.ssh/id_rsa
 
-sudo -u andock-ci chmod 600 ~/.ssh/id_rsa
+sudo -u andock-ci chmod 600 /home/andock-ci/.ssh/id_rsa
 sudo -u andock-ci eval `ssh-agent -s`
-sudo -u andock-ci ssh-add ~/.ssh/id_rsa
-sudo -u andock-ci cp ssh-config ~/.ssh/config
+sudo -u andock-ci ssh-add /home/andock-ci/.ssh/id_rsa
+sudo -u andock-ci cp ssh-config /home/andock-ci/.ssh/config
