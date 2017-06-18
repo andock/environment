b49c304397dab626031c0b500f0a6ecef8a05053
Christian Wiedemann <christian.wiedemann@key-tec.de>
2017-06-18 22:45:08 +0200

    Remove strict host checking

--- andockci_before_install.sh
+++ andockci_before_install.sh
@@ -4,7 +4,7 @@ set -e
 sudo cp /home/travis/.ssh/id_rsa /home/andock-ci/id_rsa
 sudo chown andock-ci:docker /home/andock-ci/.ssh/id_rsa
 
-ssh andock-ci@localhost
+ssh -o StrictHostKeyChecking=no andock-ci@localhost
 chmod 600 /home/andock-ci/.ssh/id_rsa
 eval `ssh-agent -s`
 ssh-add /home/andock-ci/.ssh/id_rsa
