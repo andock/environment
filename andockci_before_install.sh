1476ce976bc23f2d4655aa95e8cf4f6697509cc5
Christian Wiedemann <christian.wiedemann@key-tec.de>
2017-06-18 22:40:26 +0200

    Try ssh connect

--- andockci_before_install.sh
+++ andockci_before_install.sh
@@ -4,7 +4,7 @@ set -e
 sudo cp /home/travis/.ssh/id_rsa /home/andock-ci/id_rsa
 sudo chown andock-ci:docker /home/andock-ci/.ssh/id_rsa
 
-sudo su andock-ci
+ssh andock-ci@localhost
 chmod 600 /home/andock-ci/.ssh/id_rsa
 eval `ssh-agent -s`
 ssh-add /home/andock-ci/.ssh/id_rsa
