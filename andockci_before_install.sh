b763f270869806563863a9d97171cf8a4de2a4b4
Christian Wiedemann <christian.wiedemann@key-tec.de>
2017-06-19 07:22:38 +0200

    Fix test: Add  ssh agent via ssh

--- andockci_before_install.sh
+++ andockci_before_install.sh
@@ -7,6 +7,6 @@ sudo chmod 600 /home/andock-ci/.ssh/id_rsa
 sudo cp ssh-config /home/andock-ci/.ssh/config
 sudo chown andock-ci:docker /home/andock-ci/.ssh/config
 pwd
-ssh -o StrictHostKeyChecking=no andock-ci@localhost "/home/travis/fin/andoc_ci.sh"
+ssh -o StrictHostKeyChecking=no andock-ci@localhost "/home/travis/build/andock-ci/fin/andoc_ci.sh"
 
 
