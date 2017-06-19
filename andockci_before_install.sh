e46d6cd1ef742b163f9fa122725291c23c73a828
Christian Wiedemann <christian.wiedemann@key-tec.de>
2017-06-19 07:39:20 +0200

    Fix tests: Use sudo -u

--- andockci_before_install.sh
+++ andockci_before_install.sh
@@ -7,6 +7,7 @@ sudo chmod 600 /home/andock-ci/.ssh/id_rsa
 sudo cp ssh-config /home/andock-ci/.ssh/config
 sudo chown andock-ci:docker /home/andock-ci/.ssh/config
 pwd
-ssh -o StrictHostKeyChecking=no andock-ci@localhost "/home/travis/build/andock-ci/fin/andoc_ci.sh"
+#ssh -o StrictHostKeyChecking=no andock-ci@localhost "/home/travis/build/andock-ci/fin/andoc_ci.sh"
+sudo -u andock-ci ./andoc_ci.sh
 
 
