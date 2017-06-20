68edd306faf33eb2aaadf826a27798274872232f
Christian Wiedemann <christian.wiedemann@key-tec.de>
2017-06-20 07:24:57 +0200

    Use bash_profile

--- andockci_before_install.sh
+++ andockci_before_install.sh
@@ -6,8 +6,5 @@ sudo chown andock-ci:docker /home/andock-ci/.ssh/id_rsa
 sudo chmod 600 /home/andock-ci/.ssh/id_rsa
 sudo cp ssh-config /home/andock-ci/.ssh/config
 sudo chown andock-ci:docker /home/andock-ci/.ssh/config
-pwd
-#ssh -o StrictHostKeyChecking=no andock-ci@localhost "/home/travis/build/andock-ci/fin/andoc_ci.sh"
-sudo -u andock-ci ./andoc_ci.sh
 
 
