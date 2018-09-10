24fb7eb957df56b59a4061d07d3fa14d1f5de2d7
Christian Wiedemann <christian.wiedemann@key-tec.de>
2018-09-10 08:01:18 +0200

    Fix tests.

--- run_tests.sh
+++ run_tests.sh
@@ -10,18 +10,6 @@ config_file="tests/$2.yml"
 
 git -C demo-project pull || git clone https://github.com/andock/demo-project.git
 
-ansible-playbook -i $inventory_file $config_file --tags exec
-
-ansible-playbook -i $inventory_file $config_file --tags exec -e "exec_command=sysinfo"
-
-ansible-playbook -i $inventory_file $config_file --tags update
-
-ansible-playbook -i $inventory_file $config_file --tags test
-
-ansible-playbook -i $inventory_file $config_file --tags stop
-
-ansible-playbook -i $inventory_file $config_file --tags rm
-
   # Create the instance and check it again
 
 ansible-playbook -i $inventory_file $config_file --tags init
@@ -32,7 +20,7 @@ ls -al ~/andock/projects/demo-project/envs/demo-project--master
 
 if [ "$2" = "mounts" ]
 
-    then cd ~/andock/projects/demo-project/envs/demo-project--master/docroot/files; touch test.txt
+    then cd ~/andock/projects/demo-project/demo-project--master__env/docroot/files; touch test.txt
     exit
 fi
 ansible-playbook -i $inventory_file $config_file --tags update
