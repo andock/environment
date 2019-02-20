493bb84a0515c12aafcbe2410870bb4a41559d78
Christian Wiedemann <christian.wiedemann@key-tec.de>
2019-02-20 15:32:51 +0100

    Fix tests.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -4,6 +4,6 @@ set -e
 git -C test-project pull || git clone https://github.com/andock/test-project.git
 
 mkdir -p ~/.andock
-cp version.env ~/andock/version.env
+cp version.env ~/.andock/version.env
 bats environment.bats
 
